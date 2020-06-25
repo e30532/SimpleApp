42d896fced74993cab7fb7034308ee4699e8f975
e30532 <e30532@jp.ibm.com>
2020-06-25 02:19:07 -0700

    modify the servlet code

--- SimpleWXSWeb/src/simple/wxs/SimpleServletLiberty.java
+++ SimpleWXSWeb/src/simple/wxs/SimpleServletLiberty.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.websphere.objectgrid.CatalogDomainInfo;
+import com.ibm.websphere.objectgrid.CatalogDomainManager;
 import com.ibm.websphere.objectgrid.ClientClusterContext;
 import com.ibm.websphere.objectgrid.ConnectException;
 import com.ibm.websphere.objectgrid.ElasticCacheClientConnectionHelper;
@@ -18,6 +20,8 @@ import com.ibm.websphere.objectgrid.ObjectGridManagerFactory;
 import com.ibm.websphere.objectgrid.ObjectMap;
 import com.ibm.websphere.objectgrid.Session;
 import com.ibm.websphere.objectgrid.security.config.ClientSecurityConfiguration;
+import com.ibm.websphere.objectgrid.security.config.ClientSecurityConfigurationFactory;
+import com.ibm.websphere.objectgrid.security.plugins.builtins.UserPasswordCredentialGenerator;
 
 /**
  * Servlet implementation class SimpleServletLiberty
@@ -40,8 +44,37 @@ public class SimpleServletLiberty extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ObjectGrid aObjGrid = null;
 		try {
-			aObjGrid = ElasticCacheClientConnectionHelper.connect("testGrid1");
-			Session session = aObjGrid.getSession();
+			
+			//https://www.ibm.com/support/knowledgecenter/en/SSTVLU_8.6.1/com.ibm.websphere.extremescale.doc/txs_authecaas.html
+			/**
+			 <elasticCacheClient id="testGrid1" elasticCacheClientSecurityRef="securitySettings">
+               <elasticCacheCatalogCluster>
+                 <server host="vlrh1" port="4809" />
+               </elasticCacheCatalogCluster>
+             </elasticCacheClient>
+             <elasticCacheClientSecurity id="securitySettings"
+                                enabled="true"
+                                credentialAuthentication="Required"
+                                credentialGeneratorClass="com.ibm.websphere.objectgrid.security.plugins.builtins.UserPasswordCredentialGenerator"
+                                credentialGeneratorProps="username password"
+                                transportType="TCP/IP">
+             </elasticCacheClientSecurity>
+			**/
+			ObjectGridManager ogManager= ObjectGridManagerFactory.getObjectGridManager();
+		    CatalogDomainManager domainManager = ogManager.getCatalogDomainManager();
+		    CatalogDomainInfo domainInfo = domainManager.getDomainInfo("testGrid1"); //This should be id of elasticCacheClient element in server.xml
+		    String endpoints = domainInfo.getClientCatalogServerEndpoints();
+		    ClientSecurityConfiguration csc = domainInfo.getClientSecurityConfiguration();
+		    ClientClusterContext ccc = ogManager.connect(endpoints, csc, null);
+		    aObjGrid = ogManager.getObjectGrid(ccc, "testGrid1");
+		    
+
+		    //If security is not enabled,  ElasticCacheClientConnectionHelper can be used.
+		    //https://www.ibm.com/support/knowledgecenter/en/SSTVLU_8.6.1/com.ibm.websphere.extremescale.doc/txsjavaclientliberty.html
+			//aObjGrid = ElasticCacheClientConnectionHelper.connect("testGrid1");
+
+		    
+		    Session session = aObjGrid.getSession();
 			ObjectMap om = session.getMap("testGrid1");
 			
 			if(request.getParameter("operation").equals("add")) {
