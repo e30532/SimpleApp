d81046989e324667370e92dc08628cd633d219d2
e30532 <e30532@jp.ibm.com>
2020-11-16 19:51:52 -0800

    create a simple jdbc application

---
+++ SimpleJDBCWeb/src/simple/jdbc/SimpleServlet.java
@@ -0,0 +1,86 @@
+package simple.jdbc;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+                
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+ 
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+                System.out.println("simple.jdbc.SimpleServlet.doGet()>");
+                String sql = request.getParameter("sql");
+                String operation = request.getParameter("operation");
+        
+                DataSource ds = null;
+                Connection con = null;
+                Statement stmt = null;
+                ResultSet rs = null;
+                
+                try {
+                        InitialContext initCtx = new InitialContext();
+                        ds = (DataSource) initCtx.lookup("jdbc/SimpleDS");
+                        con = ds.getConnection();
+                        stmt = con.createStatement();
+                        if(operation.equals("select")) {
+                                rs = stmt.executeQuery(sql);
+                                while(rs.next()){
+                                        int id = rs.getInt("ID");
+                                        String name = rs.getString("NAME");
+                                        System.out.println("ID: " + id + ". Name: " + name);
+                                }
+                        }else if(operation.equals("update")) {
+                                int i = stmt.executeUpdate(sql);
+                                System.out.println(i + " record was updated.");
+                        }else if(operation.equals("insert")){
+                                int i = stmt.executeUpdate(sql);
+                                System.out.println(i + " record was inserted.");
+                        }else if(operation.equals("delete")){
+                                int i = stmt.executeUpdate(sql);
+                                System.out.println(i + " record was deleted.");
+                        }
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+                        try {
+                                if (rs != null) rs.close();
+                                if (stmt != null) stmt.close();
+                                if (con != null) con.close();
+                        } catch (SQLException e) {
+                                        e.printStackTrace();
+                        }
+                }
+                getServletContext().getRequestDispatcher("index.jsp").forward(request, response);
+                System.out.println("simple.jdbc.SimpleServlet.doGet()<");
+        }
+
+
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                doGet(request, response);
+        }
+
+}
